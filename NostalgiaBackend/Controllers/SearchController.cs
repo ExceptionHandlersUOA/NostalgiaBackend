60f2df56cc10cf48b1b28d8039059d878bf06faf
FeroxFoxxo <feroxfoxxo@gmail.com>
2025-08-03 10:21:06 +1200

    feat: created search endpoint

---
+++ NostalgiaBackend/Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shared.Database;
+using Shared.Models;
+
+namespace NostalgiaBackend.Controllers
+{
+    [ApiController]
+    [Route("api/search")]
+    public class SearchController(PostContext context) : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<List<Post>>> Search([FromQuery] string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return BadRequest("Search query is required");
+            }
+
+            var posts = await context.Posts.Where(p => p.Categories.Contains(query) || p.SourceUrl.Contains(query) ||
+                p.Title.Contains(query) || p.Body.Contains(query) || p.Description.Contains(query)).ToListAsync();
+
+            return Ok(posts);
+        }
+    }
+}
