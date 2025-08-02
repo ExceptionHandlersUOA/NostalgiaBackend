d7916abf1afff016c7596bdccb842b9dceca55fb
FeroxFoxxo <feroxfoxxo@gmail.com>
2025-08-02 17:16:40 +1200

    chore: generate migrations

---
+++ Shared/Migrations/20250802051613_FeedPlatform.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class FeedPlatform : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Platform",
+                table: "Posts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Platform",
+                table: "Feeds",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Platform",
+                table: "Feeds");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Platform",
+                table: "Posts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
