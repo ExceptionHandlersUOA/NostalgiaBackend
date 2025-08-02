0287cc035cc1136a8bc6fb906c522769f5a222ee
FeroxFoxxo <feroxfoxxo@gmail.com>
2025-08-02 16:29:52 +1200

    fix: migrate instead of create

--- NostalgiaBackend/Program.cs
+++ NostalgiaBackend/Program.cs
@@ -57,11 +57,9 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<PostContext>();
-    context.Database.EnsureCreated();
     context.Database.Migrate();
     DbInitializer.Initialize(context);
 }
-
 
 app.UseCors();
 
