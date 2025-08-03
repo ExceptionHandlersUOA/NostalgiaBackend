41d46caf9db1bdf58e742a636a8fd5a75a0a58e0
FeroxFoxxo <feroxfoxxo@gmail.com>
2025-08-03 16:02:20 +1200

    fix: trycatch spam

--- HoverthArchiver/Instagram.cs
+++ HoverthArchiver/Instagram.cs
@@ -14,20 +14,11 @@ public static class Instagram
 {
     private static IInstaApi _instaApi;
 
-    private static string GetExtension(string url)
-    {
-        return url.Split('?').First().TrimEnd('/').Split('/').Last().Split('.').Last();
-    }
-
     public static async Task<Feed> AddInstagram(string url)
     {
-        //var credname = "ttteeeeesssstt";
-        //var cred = System.Text.Encoding.UTF8.GetString(
-        //    System.Convert.FromBase64String("RXZ1VFhuZnB0WU9ETnBEQkwwaDZuWDRyUHY4V3ZrVGI="));
+        var credname = Environment.GetEnvironmentVariable("INSTAGRAM_USERNAME");
+        var cred = Environment.GetEnvironmentVariable("INSTAGRAM_PASSWORD");
 
-        var credname = System.Environment.GetEnvironmentVariable("INSTAGRAM_USERNAME");
-        var cred = System.Environment.GetEnvironmentVariable("INSTAGRAM_PASSWORD");
-        
         _instaApi ??= InstaApiBuilder.CreateBuilder()
             .SetUser(UserSessionData.ForUsername(credname).WithPassword(cred))
             .UseLogger(new DebugLogger(LogLevel.All))
@@ -35,16 +26,23 @@ public static class Instagram
             .Build();
 
         await _instaApi.LoginAsync(true);
+
         Thread.Sleep(1000);
+
         if (!_instaApi.IsUserAuthenticated) throw new Exception("User not authenticated");
 
         HoverthInput h = new HoverthInput(null, null);
+
         Console.WriteLine(_instaApi.IsUserAuthenticated);
+
         var username = url.Split('?').First().TrimEnd('/').Split('/').LastOrDefault();
         var userMedia =
             await _instaApi.UserProcessor.GetUserMediaAsync(username, PaginationParameters.MaxPagesToLoad(100));
+
         Thread.Sleep(1000);
+
         var user = (await _instaApi.UserProcessor.GetUserAsync(username)).Value;
+
         Thread.Sleep(1000);
 
         List<Post> posts = [];
@@ -52,69 +50,87 @@ public static class Instagram
         var imageUrl = "";
         var description = "";
 
-        if (user != null)
+        try
         {
-            var fullUser = (await _instaApi.UserProcessor.GetFullUserInfoAsync(user.Pk)).Value;
-
-            if (!userMedia.Succeeded) throw new Exception("Could not get user media");
-
-            foreach (var media in userMedia.Value)
+            if (user != null)
             {
-                Thread.Sleep(1000);
-                List<Media> mediaItems = [];
+                var fullUser = (await _instaApi.UserProcessor.GetFullUserInfoAsync(user.Pk)).Value;
 
-                foreach (var item in media.Images)
+                if (!userMedia.Succeeded) throw new Exception("Could not get user media");
+
+                foreach (var media in userMedia.Value)
                 {
-                    if (item.Uri == null) continue;
-                    var filename = await h.DownloadFile(item.Uri);
-                    var mediaItem = new Media()
+                    try
                     {
-                        Type = FileType.Image,
-                        FileName = filename,
-                    };
-                    mediaItems.Add(mediaItem);
+                        Thread.Sleep(500);
+                        List<Media> mediaItems = [];
+
+                        try
+                        {
+                            foreach (var item in media.Images)
+                            {
+                                if (item.Uri == null) continue;
+                                var filename = await h.DownloadFile(item.Uri);
+                                var mediaItem = new Media()
+                                {
+                                    Type = FileType.Image,
+                                    FileName = filename,
+                                };
+                                mediaItems.Add(mediaItem);
+                            }
+                        }
+                        catch (Exception) { }
+
+
+                        try
+                        {
+                            foreach (var item in media.Videos)
+                            {
+                                if (item.Uri == null) continue;
+                                var filename = await h.DownloadFile(item.Uri);
+                                var mediaItem = new Media()
+                                {
+                                    Type = FileType.Video,
+                                    FileName = filename,
+                                };
+                                mediaItems.Add(mediaItem);
+                            }
+
+                        }
+                        catch (Exception) { }
+
+                        Console.WriteLine("Processing item:" + media.Title + " - " + media.Code);
+                        var post = new Post
+                        {
+                            Body = media.Caption.Text,
+                            Title = media.Title,
+                            Description = string.Empty,
+                            PublishedAt = media.DeviceTimeStamp,
+                            Media = mediaItems,
+                        };
+
+                        posts.Add(post);
+                    }
+                    catch (Exception) { }
                 }
 
-                foreach (var item in media.Videos)
+                if (user.ProfilePicUrl != null)
                 {
-                    if (item.Uri == null) continue;
-                    var filename = await h.DownloadFile(item.Uri);
-                    var mediaItem = new Media()
-                    {
-                        Type = FileType.Video,
-                        FileName = filename,
-                    };
-                    mediaItems.Add(mediaItem);
+                    imageUrl = await h.DownloadFile(user.ProfilePicUrl);
                 }
 
-                Console.WriteLine("Processing item:" + media.Title + " - " + media.Code);
-                var post = new Post
+                if (fullUser != null)
                 {
-                    Body = media.Caption.Text,
-                    Title = media.Title,
-                    Description = string.Empty,
-                    PublishedAt = media.DeviceTimeStamp,
-                    Media = mediaItems,
-                };
-
-                posts.Add(post);
+                    description = fullUser.UserDetail.Biography ?? string.Empty;
+                }
             }
-
-            if (user.ProfilePicUrl != null)
+            else
             {
-                imageUrl = await h.DownloadFile(user.ProfilePicUrl);
-            }
-
-            if (fullUser != null)
-            {
-                description = fullUser.UserDetail.Biography ?? string.Empty;
+                Console.WriteLine("Failed to get user!");
             }
         }
-        else
-        {
-            Console.WriteLine("Failed to get user!");
-        }
-        
+        catch (Exception) { }
+
         var feed = new Feed()
         {
             Title = "@" + username,
